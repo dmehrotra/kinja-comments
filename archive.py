e0f651d863e81773ef250b4112fc315a79a9966d
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2019-11-04 13:04:35 -0500

    archive

--- archive.py
+++ archive.py
@@ -2,14 +2,9 @@ from flow import *
 from site import *
 import time
 links = get_sitemap_links(limit(),get_site())
-h = []
 for l in links:
 	for link_date in get_articles(l):
-		if ".com/" in link_date['link']:
+		if ".com/" in link_date['link'] and ".com/c" not in link_date['link']:
 			time.sleep(5)
 			l = archive(link_date)
-			h.append(l)
 
-
-with open('data.json', 'w') as outfile:
-    json.dump(h, outfile)
